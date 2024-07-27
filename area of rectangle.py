791a0a448e3e2ce36c803dc917c266b8bbba3d7e
Katukusumanth333 <157560630+Katukusumanth333@users.noreply.github.com>
2024-07-27 13:18:20 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,6 @@
+def calculate_area(length, width):
+    return length * width
+length = 5
+width = 3
+area = calculate_area(length, width)
+print("The area of the rectangle is:", area)
---
+++ area of square.py
@@ -0,0 +1,5 @@
+def calculate_area_square(side_length):
+    return side_length ** 2
+side_length = 5
+area = calculate_area_square(side_length)
+print(f"The area of the square with side length {side_length} is: {area}")
