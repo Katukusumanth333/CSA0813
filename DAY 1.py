a413eb986ab762a1cb4b1dc6a90d4a81f0790054
Katukusumanth333 <157560630+Katukusumanth333@users.noreply.github.com>
2024-08-02 13:47:16 +0530

    Add files via upload

---
+++ DAY 1.py
@@ -0,0 +1,131 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Welcome to python")
+Welcome to python
+print("python student welcome")
+python student welcome
+print("python")
+python
+r=10
+area=r*3.14
+print("area")
+area
+r=10
+arae=r*r*3.14
+print("area")
+area
+l=4
+b=4
+area=l*b
+print(area)
+16
+s=5
+area=s*s
+print(area)
+25
+num=[1,2,3,4,5,6]
+num
+[1, 2, 3, 4, 5, 6]
+num.Append(5)
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    num.Append(5)
+AttributeError: 'list' object has no attribute 'Append'. Did you mean: 'append'?
+list1=["python",7.78, 101, "hello"]
+list2["sumanth",12,34,56,]
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    list2["sumanth",12,34,56,]
+NameError: name 'list2' is not defined. Did you mean: 'list1'?
+list1=["python",7.78, 101, "hello"]
+list2=["sumanth",6.78,9]
+print(list1[0])
+python
+print(list1[2])
+101
+print(list2[0])
+sumanth
+print(list2[2])
+9
+print(list1[1;3])
+SyntaxError: invalid syntax
+print(list1[1:3])
+[7.78, 101]
+print(list2[1:])
+[6.78, 9]
+list2*3
+['sumanth', 6.78, 9, 'sumanth', 6.78, 9, 'sumanth', 6.78, 9]
+list1[2]=23
+print(list)
+<class 'list'>
+list1[2]=45
+print(list1)
+['python', 7.78, 45, 'hello']
+list.insert(2,"program")
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    list.insert(2,"program")
+TypeError: descriptor 'insert' for 'list' objects doesn't apply to a 'int' object
+list1.insert(2,"program")
+print(list1)
+['python', 7.78, 'program', 45, 'hello']
+list1.remove(45)
+print(list1)
+['python', 7.78, 'program', 'hello']
+tupple=("python", 7.78, 101, "hello")
+print(t[0])
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(t[0])
+NameError: name 't' is not defined
+print(t[0])
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    print(t[0])
+NameError: name 't' is not defined
+print(tupple[0])
+python
+food={"ham":"yes", "egg":"yes", "rate":450}
+print(food)
+{'ham': 'yes', 'egg': 'yes', 'rate': 450}
+print(food["rate"])
+450
+print(food["egg"])
+yes
+>>> a=100
+>>> b=23
+>>> print(a+b)
+123
+>>> print(a-b)
+77
+>>> print(a*b)
+2300
+>>> print(a/b)
+4.3478260869565215
+>>> print(a%b)
+8
+>>> print(a**b)
+10000000000000000000000000000000000000000000000
+>>> print(a//b)
+4
+>>> print(a==b)
+False
+>>> print(a>b)
+True
+>>> print(a<b)
+False
+>>> print(a<=b)
+False
+>>> print(a>=b)
+True
+>>> print(a!=b)
+True
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
